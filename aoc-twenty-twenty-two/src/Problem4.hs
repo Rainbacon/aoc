849624fdc0a40353bb5119e3c773a8bd9b53aa48
Rainbacon <reynolds.aaron1992@gmail.com>
2022-12-05 19:38:07 -0500

    day 4

--- aoc-twenty-twenty-two/src/Problem4.hs
+++ aoc-twenty-twenty-two/src/Problem4.hs
@@ -1,5 +1,27 @@
 module Problem4 (run) where
 
-run :: String -> String
-run _ = ""
+import qualified Data.Set as S
 
+type Pair = (S.Set Int, S.Set Int)
+
+runEasy :: String -> String
+runEasy = show . length . (filter (== True)) . (map (determineOverlap . parse)) . lines
+
+parse :: String -> Pair
+parse s = let first = takeWhile (/= ',') s
+              second = tail $ dropWhile (/= ',') s
+          in (parseSingle first, parseSingle second)
+
+parseSingle :: String -> S.Set Int
+parseSingle s = let first = takeWhile (/= '-') s
+                    second = tail $ dropWhile (/= '-') s
+                in S.fromList [(read first)..(read second)]
+
+determineOverlap :: Pair -> Bool
+determineOverlap (f, s) = (S.isSubsetOf f s) || (S.isSubsetOf s f)
+
+determinePartialOverlap :: Pair -> Bool
+determinePartialOverlap (f, s) = not $ S.disjoint f s
+
+run :: String -> String
+run = show . length . (filter (== True)) . (map (determinePartialOverlap. parse)) . lines
