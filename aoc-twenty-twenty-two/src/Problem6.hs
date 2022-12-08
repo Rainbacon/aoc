3d09bedf179f0dc7f4bafea4c6de87e85fd6bb69
Aaron Reynolds <aaron.reynolds@orchard.com>
2022-12-07 22:56:26 -0500

    day 6

--- aoc-twenty-twenty-two/src/Problem6.hs
+++ aoc-twenty-twenty-two/src/Problem6.hs
@@ -1,5 +1,35 @@
-module Problem6 (run) where
+module Problem6  where
 
-run :: String -> String
-run _ = ""
+import qualified Data.List as L
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Utils
 
+type Signal = (String, String, String)
+
+runEasy :: FilePath -> IO String
+runEasy fp = do
+    input <- parseFile (parseInput 4) fp
+    return (processInput input)
+
+runHard :: FilePath -> IO String
+runHard fp = do
+    input <- parseFile (parseInput 14) fp
+    return (processInput input)
+
+processInput :: Signal -> String
+processInput (n, s, r) = show $ (length n) + (length s)
+
+parseInput :: (Monad m) => Int -> ParsecT Void String m Signal
+parseInput n = do
+              noise <- manyTill letterChar (try $ lookAhead $ parseStart n)
+              start <- parseStart n
+              rest <- some letterChar
+              return (noise, start, rest)
+
+parseStart :: (Monad m) => Int -> ParsecT Void String m String
+parseStart n = do
+              letters <- count n letterChar
+              let allDiff = L.nub letters == letters
+              if allDiff then return letters else fail "some letters are the same"
