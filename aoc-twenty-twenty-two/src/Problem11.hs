a8606530240ff35cc2a7c8a3c5dae3256f3e4720
Rainbacon <reynolds.aaron1992@gmail.com>
2022-12-13 07:35:05 -0500

    more days

--- aoc-twenty-twenty-two/src/Problem11.hs
+++ aoc-twenty-twenty-two/src/Problem11.hs
@@ -1,7 +1,115 @@
-module Problem11 (runEasy, runHard) where
+module Problem11 where
+
+import qualified Control.Monad.State as ST
+import qualified Data.List as L
+import qualified Data.Map as M
+import qualified Data.Maybe as Y
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Utils
+
+type Item = Integer
+data Monkey = Monkey [Item] (Integer -> Integer) (Integer -> Bool) (M.Map Bool Integer)
+type Monkies = M.Map Integer Monkey
+type ItemsInspected = M.Map Integer Integer
+type ProgramState = (Monkies, ItemsInspected, Integer -> Integer)
+
+instance Show Monkey where
+    show (Monkey items _ _ conditions) = "items: " ++ show items ++ " | conditions: " ++ show conditions ++ "\n"
 
 runEasy :: FilePath -> IO String
-runEasy _ = return ""
+runEasy fp = do
+    input <- parseFile parseInput fp
+    let initialState = (input, M.fromList (zip (M.keys input) (repeat 0)), (\x -> x `div` 3))
+    let (_, itemsInspected, _) = ST.execState (chaseMonkies 20) initialState
+    return $ show $ product $ take 2 $ reverse $ L.sort $ M.elems itemsInspected
 
 runHard :: FilePath -> IO String
-runHard _ = return ""
+runHard fp = do 
+    input <- parseFile parseInput fp
+    let initialState = (input, M.fromList (zip (M.keys input) (repeat 0)), id)
+    let (_, itemsInspected, _) = ST.execState (chaseMonkies 10000) initialState
+    return $ show $ product $ take 2 $ reverse $ L.sort $ M.elems itemsInspected
+
+chaseMonkies :: Integer -> ST.State ProgramState ()
+chaseMonkies 0 = return ()
+chaseMonkies n = do
+    (monkies, _, _) <- ST.get
+    mapM_ runMonkey (M.keys monkies)
+    chaseMonkies (n - 1)
+
+runMonkey :: Integer -> ST.State ProgramState ()
+runMonkey n = do
+    (monkies, _, _) <- ST.get
+    let m@(Monkey items op test conditions) = Y.fromJust $ M.lookup n monkies
+    mapM_ (processItem n m) items
+    (monkies, itemsInspected, wc) <- ST.get
+    ST.put (M.adjust clearItems n monkies, itemsInspected, wc)
+
+processItem :: Integer -> Monkey -> Item -> ST.State ProgramState ()
+processItem n (Monkey _ op test conditions) item = do
+    (monkies, itemsInspected, worryCond) <- ST.get
+    let newItem = worryCond (op item)
+    let cond = test newItem
+    let newMonkey = Y.fromJust $ M.lookup cond conditions
+    let newMonkies = M.adjust (addItem newItem) newMonkey monkies
+    ST.put (newMonkies, M.adjust (+1) n itemsInspected, worryCond)
+    
+addItem :: Item -> Monkey -> Monkey
+addItem item (Monkey items op test cond) = Monkey (items ++ [item]) op test cond
+
+clearItems :: Monkey -> Monkey
+clearItems (Monkey _ op test cond) = Monkey [] op test cond
+
+-- #--------------#
+-- | Parsing Code |
+-- #--------------#
+parseInput :: (Monad m) => ParsecT Void String m Monkies
+parseInput = do
+              ms <- sepEndBy1 parseMonkey eol
+              return $ M.fromList ms
+
+parseMonkey :: (Monad m) => ParsecT Void String m (Integer, Monkey)
+parseMonkey = do
+    string "Monkey "
+    num <- parseInteger
+    char ':'
+    eol
+    string "  Starting items: "
+    items <- sepBy1 parseInteger (string ", ")
+    eol
+    string "  Operation: new = old "
+    op <- parsePlus <|> parseTimes
+    string "  Test: divisible by "
+    test <- parseInteger
+    eol
+    string "    If true: throw to monkey "
+    trueCase <- parseInteger
+    eol
+    string "    If false: throw to monkey "
+    falseCase <- parseInteger
+    eol
+    let cases = M.fromList [(True, trueCase), (False, falseCase)]
+    return (num, Monkey items op (\x -> x `mod` test == 0) cases) 
+
+parseInteger :: (Monad m) => ParsecT Void String m Integer
+parseInteger = read <$> some digitChar
+
+parsePlus :: (Monad m) => ParsecT Void String m (Integer -> Integer)
+parsePlus = do
+             string "+ "
+             val <- (some digitChar) <|> (string "old")
+             eol
+             case val of
+                "old" -> return (\x -> x + x)
+                _ -> return (+ (read val))
+
+parseTimes :: (Monad m) => ParsecT Void String m (Integer -> Integer)
+parseTimes = do
+             string "* "
+             val <- (some digitChar) <|> (string "old")
+             eol
+             case val of
+                "old" -> return (\x -> x * x)
+                _ -> return (* (read val))
--- aoc-twenty-twenty-two/src/Problem9.hs
+++ aoc-twenty-twenty-two/src/Problem9.hs
@@ -1,7 +1,107 @@
-module Problem9 (runEasy, runHard) where
+module Problem9 where
+
+import qualified Data.Set as S
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Utils
+
+
+type Position = (Int, Int)
+data Rope = Rope [Position]
+data Dir = R | L | U | D
+    deriving (Read)
+type Move = (Dir, Int)
+type Visited = S.Set Position
+type Screen = [String]
+
+instance Show Rope where
+    show (Rope ps) = let screen = replicate 21 (replicate 26 '.')
+                     in unlines $ foldl setPixel screen (zip [0..] ps)
+
+showTail :: Visited -> String
+showTail v = let screen = replicate 26 (replicate 21 '.')
+             in unlines $ foldl setPixel screen (zip [0..] (S.elems v))
+
+setPixel :: Screen -> (Int, Position) -> Screen
+setPixel s (n, (x, y)) = let row = s !! x
+                             newRow = (take y row) ++ "#" ++ (drop (y + 1) row)
+                             alreadySet = (row !! y) /= '.'
+                         in case alreadySet of
+                            False -> (take x s) ++ [newRow] ++ (drop (x + 1) s)
+                            True -> s
 
 runEasy :: FilePath -> IO String
-runEasy _ = return ""
+runEasy fp = do
+    input <- parseFile parseInput fp
+    let init = Rope [(0,0), (0,0)]
+    let visited = S.empty
+    return $ show $ S.size . snd $ foldl runMove (init, visited) input
 
 runHard :: FilePath -> IO String
-runHard _ = return ""
+runHard fp = do
+    input <- parseFile parseInput fp
+    let init = Rope (replicate 10 (15,11))
+    let visited = S.empty
+    return $ show $ S.size . snd $ foldl runMove (init, visited) input
+
+parseInput :: (Monad m) => ParsecT Void String m [Move]
+parseInput = sepEndBy1 parseMove eol
+         where parseMove = do
+                            dir <- parseD <|> parseU <|> parseR <|> parseL
+                            char ' '
+                            dis <- parseInt
+                            return (dir, dis)
+
+parseInt :: (Monad m) => ParsecT Void String m Int
+parseInt = read <$> some digitChar
+
+parseD :: (Monad m) => ParsecT Void String m Dir
+parseD = do
+          char 'D'
+          return D
+
+parseU :: (Monad m) => ParsecT Void String m Dir
+parseU = do
+          char 'U'
+          return U
+
+parseL :: (Monad m) => ParsecT Void String m Dir
+parseL = do
+          char 'L'
+          return L
+
+parseR :: (Monad m) => ParsecT Void String m Dir
+parseR = do
+          char 'R'
+          return R
+
+runMove :: (Rope, Visited) -> Move -> (Rope, Visited)
+runMove s (d, n) = foldl (runMove' d) s [1..n]
+               where runMove' dir (r, v) _ = let newR@(Rope rs) = move r dir
+                                                 newT = head $ reverse rs
+                                             in (newR, S.insert newT v)
+
+move :: Rope -> Dir -> Rope
+move (Rope []) d = error "rope is empty"
+move (Rope ((xh, yh):rs)) d = let newHead = case d of
+                                    U -> (xh, yh + 1) 
+                                    D -> (xh, yh - 1) 
+                                    L -> (xh - 1, yh) 
+                                    R -> (xh + 1, yh) 
+                              in Rope $ scanl touch newHead rs
+
+touch :: Position -> Position -> Position
+touch (x1, y1) (x2, y2) | x1 - x2 > 1 && y1 == y2 = (x1 - 1, y2)
+                        | x1 - x2 > 1 && y1 > y2 = (x1 - 1, y2 + 1)
+                        | x1 - x2 > 1 && y1 < y2 = (x1 - 1, y2 - 1)
+                        | x2 - x1 > 1 && y1 == y2 = (x1 + 1, y2)
+                        | x2 - x1 > 1 && y1 > y2 = (x1 + 1, y2 + 1)
+                        | x2 - x1 > 1 && y1 < y2 = (x1 + 1, y2 - 1)
+                        | y1 - y2 > 1 && x1 == x2 = (x1, y1 - 1)
+                        | y1 - y2 > 1 && x1 > x2 = (x2 + 1, y1 - 1)
+                        | y1 - y2 > 1 && x1 < x2 = (x2 - 1, y1 - 1)
+                        | y2 - y1 > 1 && x1 == x2 = (x1, y1 + 1)
+                        | y2 - y1 > 1 && x1 > x2 = (x2 + 1, y1 + 1)
+                        | y2 - y1 > 1 && x1 < x2 = (x2 - 1, y1 + 1)
+                        | otherwise = (x2, y2)
