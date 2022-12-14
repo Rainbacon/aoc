9c821ee8d50941e58f260878451b1ec58aeabb33
Aaron Reynolds <aaron.reynolds@orchard.com>
2022-12-13 21:43:01 -0500

    Day 11

--- aoc-twenty-twenty-two/src/Problem11.hs
+++ aoc-twenty-twenty-two/src/Problem11.hs
@@ -9,11 +9,11 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import Utils
 
-type Item = Integer
-data Monkey = Monkey [Item] (Integer -> Integer) (Integer -> Bool) (M.Map Bool Integer)
-type Monkies = M.Map Integer Monkey
-type ItemsInspected = M.Map Integer Integer
-type ProgramState = (Monkies, ItemsInspected, Integer -> Integer)
+type Item = Int
+data Monkey = Monkey [Item] (Int -> Int) Int (M.Map Bool Int)
+type Monkies = M.Map Int Monkey
+type ItemsInspected = M.Map Int Int
+type ProgramState = (Monkies, ItemsInspected, Int, Int -> Int)
 
 instance Show Monkey where
     show (Monkey items _ _ conditions) = "items: " ++ show items ++ " | conditions: " ++ show conditions ++ "\n"
@@ -21,40 +21,45 @@ instance Show Monkey where
 runEasy :: FilePath -> IO String
 runEasy fp = do
     input <- parseFile parseInput fp
-    let initialState = (input, M.fromList (zip (M.keys input) (repeat 0)), (\x -> x `div` 3))
-    let (_, itemsInspected, _) = ST.execState (chaseMonkies 20) initialState
+    let supermod = foldr modProd 1 $ M.elems input
+    let initialState = (input, M.fromList (zip (M.keys input) (repeat 0)), supermod, \x -> x `div` 3)
+    let (_, itemsInspected, _, _) = ST.execState (chaseMonkies 20) initialState
     return $ show $ product $ take 2 $ reverse $ L.sort $ M.elems itemsInspected
 
 runHard :: FilePath -> IO String
 runHard fp = do 
     input <- parseFile parseInput fp
-    let initialState = (input, M.fromList (zip (M.keys input) (repeat 0)), id)
-    let (_, itemsInspected, _) = ST.execState (chaseMonkies 10000) initialState
+    let supermod = foldr modProd 1 $ M.elems input
+    let initialState = (input, M.fromList (zip (M.keys input) (repeat 0)), supermod, id)
+    let (_, itemsInspected, _, _) = ST.execState (chaseMonkies 10000) initialState
     return $ show $ product $ take 2 $ reverse $ L.sort $ M.elems itemsInspected
 
-chaseMonkies :: Integer -> ST.State ProgramState ()
+modProd :: Monkey -> Int -> Int
+modProd (Monkey _ _ test _) i = i * test 
+
+chaseMonkies :: Int -> ST.State ProgramState ()
 chaseMonkies 0 = return ()
 chaseMonkies n = do
-    (monkies, _, _) <- ST.get
+    (monkies, _, _, _) <- ST.get
     mapM_ runMonkey (M.keys monkies)
     chaseMonkies (n - 1)
 
-runMonkey :: Integer -> ST.State ProgramState ()
+runMonkey :: Int -> ST.State ProgramState ()
 runMonkey n = do
-    (monkies, _, _) <- ST.get
+    (monkies, _, _, _) <- ST.get
     let m@(Monkey items op test conditions) = Y.fromJust $ M.lookup n monkies
     mapM_ (processItem n m) items
-    (monkies, itemsInspected, wc) <- ST.get
-    ST.put (M.adjust clearItems n monkies, itemsInspected, wc)
+    (monkies, itemsInspected, supermod, wc) <- ST.get
+    ST.put (M.adjust clearItems n monkies, itemsInspected, supermod, wc)
 
-processItem :: Integer -> Monkey -> Item -> ST.State ProgramState ()
+processItem :: Int -> Monkey -> Item -> ST.State ProgramState ()
 processItem n (Monkey _ op test conditions) item = do
-    (monkies, itemsInspected, worryCond) <- ST.get
-    let newItem = worryCond (op item)
-    let cond = test newItem
+    (monkies, itemsInspected, supermod, wc) <- ST.get
+    let newItem = (wc (op item)) `mod` supermod
+    let cond = (newItem `mod` test) == 0
     let newMonkey = Y.fromJust $ M.lookup cond conditions
     let newMonkies = M.adjust (addItem newItem) newMonkey monkies
-    ST.put (newMonkies, M.adjust (+1) n itemsInspected, worryCond)
+    ST.put (newMonkies, M.adjust (+1) n itemsInspected, supermod, wc)
     
 addItem :: Item -> Monkey -> Monkey
 addItem item (Monkey items op test cond) = Monkey (items ++ [item]) op test cond
@@ -70,33 +75,33 @@ parseInput = do
               ms <- sepEndBy1 parseMonkey eol
               return $ M.fromList ms
 
-parseMonkey :: (Monad m) => ParsecT Void String m (Integer, Monkey)
+parseMonkey :: (Monad m) => ParsecT Void String m (Int, Monkey)
 parseMonkey = do
     string "Monkey "
-    num <- parseInteger
+    num <- parseInt
     char ':'
     eol
     string "  Starting items: "
-    items <- sepBy1 parseInteger (string ", ")
+    items <- sepBy1 parseInt (string ", ")
     eol
     string "  Operation: new = old "
     op <- parsePlus <|> parseTimes
     string "  Test: divisible by "
-    test <- parseInteger
+    test <- parseInt
     eol
     string "    If true: throw to monkey "
-    trueCase <- parseInteger
+    trueCase <- parseInt
     eol
     string "    If false: throw to monkey "
-    falseCase <- parseInteger
+    falseCase <- parseInt
     eol
     let cases = M.fromList [(True, trueCase), (False, falseCase)]
-    return (num, Monkey items op (\x -> x `mod` test == 0) cases) 
+    return (num, Monkey items op test cases) 
 
-parseInteger :: (Monad m) => ParsecT Void String m Integer
-parseInteger = read <$> some digitChar
+parseInt :: (Monad m) => ParsecT Void String m Int
+parseInt = read <$> some digitChar
 
-parsePlus :: (Monad m) => ParsecT Void String m (Integer -> Integer)
+parsePlus :: (Monad m) => ParsecT Void String m (Int -> Int)
 parsePlus = do
              string "+ "
              val <- (some digitChar) <|> (string "old")
@@ -105,7 +110,7 @@ parsePlus = do
                 "old" -> return (\x -> x + x)
                 _ -> return (+ (read val))
 
-parseTimes :: (Monad m) => ParsecT Void String m (Integer -> Integer)
+parseTimes :: (Monad m) => ParsecT Void String m (Int -> Int)
 parseTimes = do
              string "* "
              val <- (some digitChar) <|> (string "old")
