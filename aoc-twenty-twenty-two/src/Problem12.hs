8b124b39a6fdb8aa4734a7da26dcac53f35da2f2
Rainbacon <reynolds.aaron1992@gmail.com>
2022-12-16 20:32:33 -0500

    wip day 12

--- aoc-twenty-twenty-two/src/Problem12.hs
+++ aoc-twenty-twenty-two/src/Problem12.hs
@@ -1,26 +1,31 @@
-module Problem12 (runEasy, runHard) where
+module Problem12 where
 
+import qualified Control.Monad.State as ST
 import qualified Data.Char as C
 import qualified Data.Map as M
+import qualified Data.Maybe as Y
+import qualified Data.Set as S
 import Data.Void
 import Utils
 
-type InputType = (Position, Position, M.Map Position Int)
+type InputType = (Point, Point, M.Map Point Int)
 
 runEasy :: FilePath -> IO String
 runEasy fp = do
     input <- readFile fp
-    let data = parseInput $ lines input
+    let (start, end, points) = parseInput $ lines input
+    let initialState = ([(start, 0)], S.empty)
+    return $ show $ ST.evalState (aStar end points generateNeighbors) initialState
 
 runHard :: FilePath -> IO String
 runHard _ = return ""
 
 parseInput :: [String] -> InputType
-parseInput xs = let positions = mapPosition xs
+parseInput xs = let positions = mapPos xs
                     (start, end) = foldl parseCell (Nothing, Nothing) positions
-                in (Y.fromJust start, Y.fromJust end, M.fromList $ map toHeight positions)
+                in (Y.fromJust start, Y.fromJust end, M.fromList $ map (fmap toHeight) positions)
 
-parseCell :: (Maybe Position, Maybe Position) -> (Position, Char) -> (Maybe Position, Maybe Position)
+parseCell :: (Maybe Point, Maybe Point) -> (Point, Char) -> (Maybe Point, Maybe Point)
 parseCell (s, e) (p, 'S') = (Just p, e)
 parseCell (s, e) (p, 'E') = (s, Just p)
 parseCell xs _ = xs
@@ -29,3 +34,10 @@ toHeight :: Char -> Int
 toHeight 'S' = 1
 toHeight 'E' = 26
 toHeight c = C.ord c - 96
+
+generateNeighbors :: Point -> Grid -> [(Point, Int)]
+generateNeighbors (x, y) g = let height = Y.fromJust $ M.lookup (x, y) g
+                                 points = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+                                 mappedPoints = map (\a -> (a, M.lookup a g)) points 
+                                 valid = map (\(p, mv) -> (p, fmap (\i -> abs (i - height) < 2) mv)) mappedPoints
+                             in zip (map fst $ filter (Y.fromJust . snd) $ filter (Y.isJust . snd) valid) (repeat 1)
--- aoc-twenty-twenty-two/src/Utils.hs
+++ aoc-twenty-twenty-two/src/Utils.hs
@@ -1,6 +1,16 @@
-module Utils where
+module Utils (
+    parseFile 
+  , Point
+  , Grid
+  , aStar
+  , mapPos
+) where
 
+import Debug.Trace
 import Control.Monad.IO.Class
+import Control.Monad.State as ST
+import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.Void
 import Text.Megaparsec
 
@@ -13,8 +23,40 @@ parseFile parser filepath = do
         Right x -> return x
 
 
-type Position = (Int, Int)
+type Point = (Int, Int)
+type Grid = M.Map Point Int
 
-mapPos :: [[a]] -> [(Position, a)]
-mapPos xs = zipWith mapPos' [0..] xs
-        where mapPos' i ys = zipWith (\j y -> ((i, j), y)) ys
+mapPos :: [[a]] -> [(Point, a)]
+mapPos xs = concat $ zipWith mapPos' [0..] xs
+        where mapPos' i ys = zipWith (\j y -> ((i, j), y)) [0..] ys
+
+type Visited = S.Set Point
+type PrioQueue = [(Point, Int)]
+
+aStar :: Point -> Grid -> (Point -> Grid -> [(Point, Int)]) -> ST.State (PrioQueue, Visited) Int 
+aStar target grid constructNeighbors = do
+    (queue, seen) <- ST.get
+    let d@(p@(x, y), distance) = head queue
+    if ((x, y) == target)
+    then return distance
+    else do
+        let newPaths = trace ("Point is" ++ show p) constructNeighbors (x, y) grid
+        let unseenPaths = filter (\node -> not $ S.member (fst node) seen) newPaths
+        let newQueue = insertPaths (map (fmap (+distance)) unseenPaths) $ tail queue
+        ST.put (newQueue, S.insert (x,y) seen)
+        aStar target grid constructNeighbors
+
+insertPaths :: [(Point, Int)] -> PrioQueue -> PrioQueue
+insertPaths [] q = q
+insertPaths (x:xs) q = let rest = insertPaths xs q
+                       in insertPath x False rest
+
+insertPath :: (Point, Int) -> Bool -> PrioQueue -> PrioQueue
+insertPath x False [] = [x]
+insertPath x True [] = []
+insertPath a@(p, _) True (b@(x, _):xs) | p == x = xs
+                                       | otherwise = b:(insertPath a True xs)
+insertPath a@(p, v1) inserted q@(b@(x, v2):xs) | p == x && v1 >= v2 = q
+                                               | p == x = a:(insertPath a True xs)
+                                               | v2 >= v1 = a:(insertPath a True q)
+                                               | otherwise = b:(insertPath a inserted xs)
