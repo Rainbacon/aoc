30e6dc5c0907355ff6a1320582f3af2a488bd5fc
Aaron Reynolds <aaron.reynolds@orchard.com>
2023-12-01 01:12:10 -0500

    2023 Day 1

--- src/Problems2023/Problem1.hs
+++ src/Problems2023/Problem1.hs
@@ -1,7 +1,46 @@
 module Problems2023.Problem1 (runEasy, runHard) where
 
+import Utils
+import Data.Char
+import Data.List
+import Data.Maybe
+import Debug.Trace
+
 runEasy :: FilePath -> IO String
-runEasy _ = return ""
+runEasy fp = do
+    input <- readFile fp
+    return $ show $ sum $ map toInt (lines input)
 
 runHard :: FilePath -> IO String
-runHard _ = return ""
+runHard fp = do
+    input <- readFile fp
+    let x = map toInt' (lines input)
+    return $ show $ sum $ x
+
+toInt :: String -> Int 
+toInt s = let digits = filter isDigit s
+              first = head digits
+              last = head $ reverse digits
+           in read (first:last:[])
+
+toInt' :: String -> Int
+toInt' s = let maybes = map matchDigit $ tails s
+               digits = map (fromJust) $ filter isJust maybes
+               first = head digits
+               last = head $ reverse digits
+            in 10 * first + last
+
+matchDigit :: String -> Maybe Int
+matchDigit ('z':'e':'r':'o':_) = return 0
+matchDigit ('o':'n':'e':_) = return 1
+matchDigit ('t':'w':'o':_) = return 2
+matchDigit ('t':'h':'r':'e':'e':_) = return 3
+matchDigit ('f':'o':'u':'r':_) = return 4
+matchDigit ('f':'i':'v':'e':_) = return 5
+matchDigit ('s':'i':'x':_) = return 6
+matchDigit ('s':'e':'v':'e':'n':_) = return 7
+matchDigit ('e':'i':'g':'h':'t':_) = return 8
+matchDigit ('n':'i':'n':'e':_) = return 9
+matchDigit (x:_) | isDigit x = return (read [x]) 
+                 | otherwise = Nothing
+matchDigit [] = Nothing
