1d31477005723b113323c0b59347720a587fc5bf
Rainbacon <reynolds.aaron1992@gmail.com>
2023-12-20 18:18:25 -0500

    more wip

--- src/Problems2023/Problem19.hs
+++ src/Problems2023/Problem19.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 module Problems2023.Problem19 (runEasy, runHard) where
 
 import qualified Control.Monad.State as ST
@@ -17,7 +18,7 @@ data Rule = Greater Char Int RuleResult | Less Char Int RuleResult | Default Rul
     deriving (Show)
 type Range = M.Map Char (Int, Int)
 type Mapping = (Range, RuleResult)
-type SankeyState = ([Range], Workflow)
+type SankeyState = ([Range], Workflow, [(Range, RuleResult)])
 
 isNext :: RuleResult -> Bool
 isNext (Next s) = True
@@ -39,20 +40,66 @@ runHard :: FilePath -> IO String
 runHard fp = do
     (flows, _) <- parseFile parseInput fp
     let initRange = M.fromList [('x', (0, 4000)), ('m', (0, 4000)), ('a', (0, 4000)), ('s', (0, 4000))]
-    let initState = ([], flows)
+    let initState = ([], flows, [(initRage, Next "in")])
 
-runFlow' :: String -> Workflow -> Range -> ST.State SankeyState Range
-runFlow' key range = do
-    (_, flows) <- ST.get
-    let rules = Y.fromJust $ M.lookup key flows
-    foldM splitRange range rules
+runFlows :: ST.State SankeyState Range
+runFlows = do
+    (accepted, flows, queue) <- ST.get
+    case queue of
+        [] -> return $ M.empty
+        ((range, res):qs) -> do
+            ST.put (accepted, flows, qs)
+            let key = fromNext res
+            let rules = Y.fromJust $ M.lookup key flows
+            foldM splitRange range rules
+            runFlows
 
 splitRange :: Range -> Rule -> ST.State SankeyState Range
+splitRange (M.null -> True) _ = return $ M.empty
 splitRange range (Default Reject) = return $ M.empty
 splitRange range (Default Accept) = do
-    (accepted, flows) <- ST.get
-    ST.put ((range:accepted), flows)
-splitRange range (Default (Next res)) = runFlow' res range 
+    (accepted, flows, queue) <- ST.get
+    ST.put ((range:accepted), flows, queue)
+    return $ M.empty
+splitRange range (Default res) = do
+    (accepted, flows, queue) <- ST.get
+    ST.put (accepted, flows, (range, res):queue)
+    return $ M.empty
+splitRange range (Greater c n res) = do
+    (accepted, flows, queue) <- ST.get
+    let (low, high) = Y.fromJust $ M.lookup c range
+    let expanded = [low..high]
+    let putR = filter (> n) expanded
+    let retR = filter (<= n) expanded
+    case len putR of
+        0 -> return range
+        _ -> do 
+            let newLow = head putR
+            let newHigh = head $ reverse putR
+            let newRange = M.insert c (newLow, newHigh) range
+            ST.put (accepted, flows, (newRange, res):queue)
+            let newLow' = head retR
+            let newHigh' = head $ reverse retR
+            let newRange' = M.insert c (newLow', newHigh') range
+            return newRange'
+splitRange range (Less c n res) = do
+    (accepted, flows, queue) <- ST.get
+    let (low, high) = Y.fromJust $ M.lookup c range
+    let expanded = [low..high]
+    let putR = filter (< n) expanded
+    let retR = filter (>= n) expanded
+    case len putR of
+        0 -> return range
+        _ -> do 
+            let newLow = head putR
+            let newHigh = head $ reverse putR
+            let newRange = M.insert c (newLow, newHigh) range
+            ST.put (accepted, flows, (newRange, res):queue)
+            let newLow' = head retR
+            let newHigh' = head $ reverse retR
+            let newRange' = M.insert c (newLow', newHigh') range
+            return newRange'
+
 
 scorePart :: Part -> Int
 scorePart part = sum $ M.elems part
