44f8fd9ddecb826bd930cd8c28432165aecea965
Rainbacon <reynolds.aaron1992@gmail.com>
2025-11-07 00:39:41 -0500

    still wip on day 7

--- src/Problems2015/Problem7.hs
+++ src/Problems2015/Problem7.hs
@@ -7,14 +7,18 @@ import Data.Void
 import qualified Data.Map as M
 import Data.Int
 import Data.Bits
+import Debug.Trace
+
+data GateInput = Signal Int16 | Identifier String
+    deriving (Show)
 
 data Gate =
-    Signal Int16
-  | And' String String
-  | Or String String
-  | Not String
-  | LShift String Int
-  | RShift String Int
+    And' GateInput GateInput
+  | Or GateInput GateInput
+  | Not GateInput
+  | LShift GateInput Int
+  | RShift GateInput Int
+  | Pass GateInput
     deriving (Show)
 
 type Wires = M.Map String Gate
@@ -24,19 +28,22 @@ runEasy fp = do
     wires <- parseFile parseInput fp
     putStrLn "What wire do you seek?"
     wire <- getLine
-    putStrLn $ show wires
     return $ show $ evalWire wire wires
 
 evalWire :: String -> Wires -> Int16
-evalWire wire wires = let gate = M.lookup wire wires
+evalWire wire wires = let gate = trace ("wire " ++ wire ++ " " ++ (show $ M.lookup wire wires)) $ M.lookup wire wires
     in case gate of
         Nothing -> error $ "Looked up missign gate " ++ show wire
-        (Just (Signal n)) -> n
-        (Just (And' a b)) -> (evalWire a wires) .&. (evalWire b wires)
-        (Just (Or a b)) -> (evalWire a wires) .|. (evalWire b wires)
-        (Just (Not a)) -> 65535 `xor` (evalWire a wires)
-        (Just (LShift a n)) -> shift (evalWire a wires) n
-        (Just (RShift a n)) -> shift (evalWire a wires) (n * (-1))
+        (Just (And' a b)) -> (evalWire' a wires) .&. (evalWire' b wires)
+        (Just (Or a b)) -> (evalWire' a wires) .|. (evalWire' b wires)
+        (Just (Not a)) -> 65535 `xor` (evalWire' a wires)
+        (Just (LShift a n)) -> shift (evalWire' a wires) n
+        (Just (RShift a n)) -> shift (evalWire' a wires) (n * (-1))
+        (Just (Pass a)) -> evalWire' a wires
+
+evalWire' :: GateInput -> Wires -> Int16
+evalWire' (Signal i) _ = i
+evalWire' (Identifier s) ws = evalWire s ws 
 
 runHard :: FilePath -> IO String
 runHard _ = return ""
@@ -46,44 +53,42 @@ parseInput = do
     wires <- sepEndBy1 parseWire eol
     return $ M.fromList wires
 
+parseGateInput :: (Monad m) => ParsecT Void String m GateInput
+parseGateInput = (some letterChar >>= return . Identifier) <|> (some digitChar >>= return . Signal . read)
+
 parseWire :: (Monad m) => ParsecT Void String m (String, Gate)
 parseWire = do
     gate <- parseGate
     string " -> "
-    wire <- many letterChar
+    wire <- some letterChar
     return $ (wire, gate)
 
 parseGate :: (Monad m) => ParsecT Void String m Gate
-parseGate = parseSignal <|> parseAnd <|> parseOr <|> parseNot <|> parseLShift <|> parseRShift
-
-parseSignal :: (Monad m) => ParsecT Void String m Gate
-parseSignal = do
-    sig <- many digitChar
-    return $ Signal (read sig)
+parseGate = (try parseAnd) <|> (try parseOr) <|> (try parseNot) <|> (try parseLShift) <|> (try parseRShift) <|> parsePass
 
 parseAnd :: (Monad m) => ParsecT Void String m Gate
 parseAnd = do
-    i1 <- many letterChar
+    i1 <- parseGateInput
     string " AND "
-    i2 <- many letterChar
+    i2 <- parseGateInput
     return $ And' i1 i2
 
 parseOr :: (Monad m) => ParsecT Void String m Gate
 parseOr = do
-    i1 <- many letterChar
+    i1 <- parseGateInput
     string " OR "
-    i2 <- many letterChar
+    i2 <- parseGateInput
     return $ Or i1 i2
 
 parseNot :: (Monad m) => ParsecT Void String m Gate
 parseNot = do
     string "NOT "
-    i <- many letterChar
+    i <- parseGateInput
     return $ Not i
 
 parseLShift :: (Monad m) => ParsecT Void String m Gate
 parseLShift = do
-    i <- many letterChar
+    i <- parseGateInput
     string " LSHIFT "
     n <- parseInt
     return $ LShift i n
@@ -91,7 +96,10 @@ parseLShift = do
 
 parseRShift :: (Monad m) => ParsecT Void String m Gate
 parseRShift = do
-    i <- many letterChar
+    i <- parseGateInput
     string " RSHIFT "
     n <- parseInt
     return $ RShift i n
+
+parsePass :: (Monad m) => ParsecT Void String m Gate
+parsePass = parseGateInput >>= return . Pass
