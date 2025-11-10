4c92b078487f5e3bd80d6b58c56e53481a110c45
Rainbacon <reynolds.aaron1992@gmail.com>
2025-11-09 22:22:19 -0500

    2015 day 7

--- src/Problems2015/Problem7.hs
+++ src/Problems2015/Problem7.hs
@@ -6,20 +6,21 @@ import Text.Megaparsec.Char
 import Data.Void
 import qualified Data.Map as M
 import qualified Data.Maybe as Y
-import Data.Int
+import qualified Control.Monad.State as ST
+import Data.Word
 import Data.Bits
 import Debug.Trace
 
-data GateInput = Signal Int16 | Identifier String
-    deriving (Show)
 
 data Gate =
-    And' GateInput GateInput
-  | Or GateInput GateInput
-  | Not GateInput
-  | LShift GateInput Int
-  | RShift GateInput Int
-  | Pass GateInput
+    Signal Word16
+  | Identifier String
+  | And' Gate Gate
+  | Or Gate Gate
+  | Not Gate
+  | LShift Gate Int
+  | RShift Gate Int
+  | Pass Gate
     deriving (Show)
 
 type Wires = M.Map String Gate
@@ -29,29 +30,57 @@ runEasy fp = do
     wires <- parseFile parseInput fp
     putStrLn "What wire do you seek?"
     wire <- getLine
-    return $ show $ wireMemo wire wires
+    return $ show $ ST.evalState (getOrCalculate wire) wires
 
-wireMemo :: String -> Wires -> Int16
-wireMemo wire wires = (M.map evalWire wires) M.! wire
-    where evalWire' (Signal i) _ = i
-          evalWire' (Identifier s) ws = wireMemo s ws 
-          evalWire gate = trace (show gate) $ case gate of
-            (And' a b) -> (evalWire' a wires) .&. (evalWire' b wires)
-            (Or a b) -> (evalWire' a wires) .|. (evalWire' b wires)
-            (Not a) -> 65535 `xor` (evalWire' a wires)
-            (LShift a n) -> shift (evalWire' a wires) n
-            (RShift a n) -> shift (evalWire' a wires) (n * (-1))
-            (Pass a) -> evalWire' a wires
+getOrCalculate :: String -> ST.State Wires Word16
+getOrCalculate wire = do
+    wires <- ST.get
+    case (wires M.! wire) of
+        (Signal i) -> return i
+        (Identifier g) -> getOrCalculate g
+        gate -> do
+            val <- evalGate gate
+            ST.modify (M.insert wire (Signal val))
+            return val
+
+evalGate :: Gate -> ST.State Wires Word16
+evalGate gate = case gate of
+    (Signal i) -> return i
+    (Identifier g) -> getOrCalculate g 
+    (And' a b) -> do
+        aVal <- evalGate a
+        bVal <- evalGate b
+        return $ aVal .&. bVal
+    (Or a b) -> do
+        aVal <- evalGate a
+        bVal <- evalGate b
+        return $ aVal .|. bVal
+    (Not a) -> do
+        aVal <- evalGate a
+        return $ complement aVal
+    (LShift a n) -> do 
+        aVal <- evalGate a
+        return $ shift aVal n
+    (RShift a n) -> do 
+        aVal <- evalGate a
+        return $ shift aVal (n * (-1))
+    (Pass a) -> evalGate a
 
 runHard :: FilePath -> IO String
-runHard _ = return ""
+runHard fp = do
+    wires <- parseFile parseInput fp
+    putStrLn "What wire do you seek?"
+    wire <- getLine
+    let ans = ST.evalState (getOrCalculate wire) wires
+    let newWires = M.insert "b" (Signal ans) wires
+    return $ show $ ST.evalState (getOrCalculate wire) newWires
 
 parseInput :: (Monad m) => ParsecT Void String m Wires
 parseInput = do
     wires <- sepEndBy1 parseWire eol
     return $ M.fromList wires
 
-parseGateInput :: (Monad m) => ParsecT Void String m GateInput
+parseGateInput :: (Monad m) => ParsecT Void String m Gate
 parseGateInput = (some letterChar >>= return . Identifier) <|> (some digitChar >>= return . Signal . read)
 
 parseWire :: (Monad m) => ParsecT Void String m (String, Gate)
