1cd77b6093851a32a01731de4da3cad800cb85c0
Rainbacon <reynolds.aaron1992@gmail.com>
2025-11-11 22:19:32 -0500

    2015 days 10 and 11

--- src/Problems2015/Problem10.hs
+++ src/Problems2015/Problem10.hs
@@ -1,7 +1,20 @@
 module Problems2015.Problem10 (runEasy, runHard) where
 
+import Utils
+
 runEasy :: FilePath -> IO String
-runEasy _ = return ""
+runEasy fp = do
+    input <- parseFast id fp
+    return $ show $ length $ foldl (\a _ -> expand a) input [1..40]
+
+expand :: String -> String
+expand [] = []
+expand all@(x:xs) = expanded' ++ expand xs'
+    where expanded = takeWhile (==x) all
+          xs' = dropWhile (==x) xs
+          expanded' = (show $ length expanded) ++ (x:[])
 
 runHard :: FilePath -> IO String
-runHard _ = return ""
+runHard fp = do
+    input <- parseFast id fp
+    return $ show $ length $ foldl (\a _ -> expand a) input [1..50]
--- src/Problems2015/Problem11.hs
+++ src/Problems2015/Problem11.hs
@@ -1,7 +1,38 @@
 module Problems2015.Problem11 (runEasy, runHard) where
 
+import Utils
+import Data.List
+import qualified Data.Maybe as Y
+
 runEasy :: FilePath -> IO String
-runEasy _ = return ""
+runEasy fp = do
+    input <- parseFast id fp
+    return $ reverse $ head $ filter isValid $ scanl (\a _ -> increment a) (reverse input) [0..]
+
+increment :: String -> String
+increment [] = []
+increment ('z':xs) = 'a':(increment xs)
+increment (x:xs) = n:xs
+    where n = next $ (1+) $ idx x
+
+isValid :: String -> Bool
+isValid s =  hasRun s && hasDoubles s 0
+    where hasRun s' | length s' < 3 = False
+                    | i1 - i2 == 1 && i2 - i3 == 1 = True
+                    | otherwise = hasRun $ tail s'
+                  where i1 = idx (s' !! 0)
+                        i2 = idx (s' !! 1)
+                        i3 = idx (s' !! 2)
+          hasDoubles s' n | length s' < 2 = False
+                          | (s' !! 0) == (s' !! 1) && n == 1 = True
+                          | (s' !! 0) == (s' !! 1) = hasDoubles (drop 2 s') (n + 1)
+                          | otherwise = hasDoubles (drop 1 s') n
+
+idx :: Char -> Int
+idx c = Y.fromJust $ elemIndex c "abcdefghjkmnpqrstuvwxyz"
+
+next :: Int -> Char
+next i = "abcdefghjkmnpqrstuvwxyz" !! i
 
 runHard :: FilePath -> IO String
 runHard _ = return ""
