4b9bee2fb61f617ecf8d508638f9b4f353fe9ba6
Rainbacon <reynolds.aaron1992@gmail.com>
2025-11-21 00:10:07 -0500

    2015 day 17

--- src/Problems2015/Problem17.hs
+++ src/Problems2015/Problem17.hs
@@ -1,7 +1,31 @@
 module Problems2015.Problem17 (runEasy, runHard) where
 
+import Utils
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+import Debug.Trace
+
 runEasy :: FilePath -> IO String
-runEasy _ = return ""
+runEasy fp = do
+    containers <- parseFile (sepEndBy1 parseInt eol) fp
+    return $ show $ determineWays 150 containers
+
+determineWays :: Int -> [Int] -> Int
+determineWays target [] = 0
+determineWays target (x:xs) | target == x = 1 + determineWays target xs
+                            | target < x = determineWays target xs
+                            | otherwise = determineWays (target - x) xs + determineWays target xs
 
 runHard :: FilePath -> IO String
-runHard _ = return ""
+runHard fp = do
+    containers <- parseFile (sepEndBy1 parseInt eol) fp
+    let perms = determineWays' 150 containers
+        m = minimum $ map length perms
+    return $ show $ length $ filter (==m) $ map length perms
+
+determineWays' :: Int -> [Int] -> [[Int]]
+determineWays' target [] = []
+determineWays' target (x:xs) | target == x = [[x]] ++ determineWays' target xs
+                             | target < x = determineWays' target xs
+                             | otherwise = map (x:) (determineWays' (target - x) xs) ++ (determineWays' target xs)
