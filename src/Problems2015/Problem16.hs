6d66da6ab9fc157bc8aff45f58a3812d0e09f54c
Rainbacon <reynolds.aaron1992@gmail.com>
2025-11-21 20:11:03 -0500

    2015 days 15 and 16

--- src/Problems2015/Problem16.hs
+++ src/Problems2015/Problem16.hs
@@ -1,7 +1,63 @@
 module Problems2015.Problem16 (runEasy, runHard) where
 
+import Utils
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+import qualified Data.Map as M
+import qualified Data.Maybe as Y
+
+data Sue = Sue {
+    number :: Int 
+  , attrs :: (M.Map String Int) }
+
+
+tickerTape = M.fromList [
+    ("children", 3),
+    ("cats", 7),
+    ("samoyeds", 2),
+    ("pomeranians", 3),
+    ("akitas", 0),
+    ("vizslas", 0),
+    ("goldfish", 5),
+    ("trees", 3),
+    ("cars", 2),
+    ("perfumes", 1)]
+
 runEasy :: FilePath -> IO String
-runEasy _ = return ""
+runEasy fp = do
+    sues <- parseFile (sepEndBy1 parseSue eol) fp
+    return $ show $ map number $ filter (attrMatch tickerTape) sues
+
+attrMatch :: M.Map String Int -> Sue -> Bool
+attrMatch tape (Sue _ as) = M.intersection tape as == as
 
 runHard :: FilePath -> IO String
-runHard _ = return ""
+runHard fp = do
+    sues <- parseFile (sepEndBy1 parseSue eol) fp
+    return $ show $ map number $ filter (attrMatch' tickerTape) sues
+
+attrMatch' :: M.Map String Int -> Sue -> Bool
+attrMatch' tape (Sue _ as) = all id matches
+    where matches = M.mapWithKey match as
+          match k v = case k of
+            "cats" -> v > (Y.fromJust $ M.lookup k tape)
+            "trees" -> v > (Y.fromJust $ M.lookup k tape)
+            "poweranians" -> v < (Y.fromJust $ M.lookup k tape)
+            "goldfish" -> v < (Y.fromJust $ M.lookup k tape)
+            _ -> v == (Y.fromJust $ M.lookup k tape)
+
+parseSue :: (Monad m) => ParsecT Void String m Sue
+parseSue = do
+    string "Sue "
+    num <- parseInt
+    string ": "
+    attrs <- sepBy1 parseAttr (string ", ")
+    return $ Sue num (M.fromList attrs)
+
+parseAttr :: (Monad m) => ParsecT Void String m (String, Int)
+parseAttr = do
+    attr <- some letterChar
+    string ": "
+    val <- parseInt
+    return (attr, val)
