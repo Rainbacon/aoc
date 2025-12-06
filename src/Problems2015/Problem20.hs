ffdde4632d92c112653afd9807631dbfa21ac2b8
Rainbacon <reynolds.aaron1992@gmail.com>
2025-12-05 20:31:07 -0500

    work on 2015 days 19 and 20

--- src/Problems2015/Problem20.hs
+++ src/Problems2015/Problem20.hs
@@ -1,7 +1,17 @@
 module Problems2015.Problem20 (runEasy, runHard) where
 
+import Utils.Parsing
+
 runEasy :: FilePath -> IO String
-runEasy _ = return ""
+runEasy fp = do
+    target <- parseFile parseInt fp
+    return $ show $ head $ dropWhile ((< target) . snd) $ map calcPresents [1..]
+
+calcPresents :: Int -> (Int, Int)
+calcPresents house = (house, sum $ map (10*) $ factorize house)
+
+factorize :: Int -> [Int]
+factorize i = (i:) $ filter (\x -> i `mod` x == 0) [1..i `div` 2]
 
 runHard :: FilePath -> IO String
 runHard _ = return ""
