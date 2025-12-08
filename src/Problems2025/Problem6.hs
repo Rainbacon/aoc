527548dfe59927a318b2a6b21d7bf2eb44a50418
Rainbacon <reynolds.aaron1992@gmail.com>
2025-12-07 19:43:04 -0500

    2025 day 6 and wip 7

--- src/Problems2025/Problem6.hs
+++ src/Problems2025/Problem6.hs
@@ -1,7 +1,61 @@
 module Problems2025.Problem6 (runEasy, runHard) where
 
+import Utils.Parsing
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+import qualified Data.Maybe as Y
+import Control.Applicative (liftA2)
+import Text.Read
+import Debug.Trace
+
+type BinOp = Int -> Int -> Int
+
 runEasy :: FilePath -> IO String
-runEasy _ = return ""
+runEasy fp = do
+    (nums, ops) <- parseFile parseProblems fp
+    return $ show $ sum $ foldl1 (zipWith3 applyBin ops) nums
+
+applyBin :: BinOp -> Int -> Int -> Int
+applyBin op x y = op x y
 
 runHard :: FilePath -> IO String
-runHard _ = return ""
+runHard fp = do
+    input <- parseFast lines fp
+    let opStr = head $ reverse input
+    let numStrs = reverse $ tail $ reverse input
+    ops <- runParserT (parseRow parseOp) "2025-day-6" opStr
+    case ops of
+        Left e -> error $ "Failed to parse: " ++ show e
+        Right x -> return $ show $ foldl1 (liftA2 (+)) $ applyFns x $ toInts numStrs
+    
+
+toInts :: [String] -> [Maybe Int]
+toInts [] = []
+toInts ([]:xs) = []
+toInts xs = mInt:toInts rest
+    where mInt = readMaybe (map head xs)
+          rest = map tail xs
+
+applyFns :: [BinOp] -> [Maybe Int] -> [Maybe Int]
+applyFns [] _ = []
+applyFns _ [] = []
+applyFns (op:ops) xs = val : applyFns ops rest
+    where val = foldl1 (liftA2 op) vals
+          vals = takeWhile Y.isJust xs
+          rest = tail $ dropWhile Y.isJust xs
+
+parseProblems :: (Monad m) => ParsecT Void String m ([[Int]], [BinOp])
+parseProblems = do
+    nums <- sepEndBy1 (parseRow parseInt) eol
+    ops <- parseRow parseOp
+    return (nums, ops)
+
+parseRow :: (Monad m) => ParsecT Void String m a -> ParsecT Void String m [a]
+parseRow p = do 
+    many (char ' ')
+    items <- sepEndBy1 p (some (char ' ')) 
+    return items
+
+parseOp :: (Monad m) => ParsecT Void String m BinOp
+parseOp = (char '*' >> pure (*)) <|> (char '+' >> pure (+))
--- src/Problems2025/Problem7.hs
+++ src/Problems2025/Problem7.hs
@@ -1,7 +1,36 @@
 module Problems2025.Problem7 (runEasy, runHard) where
 
+import Utils.Grid
+import Utils.Parsing
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+data Manifold = Start | Split
+
+isStart :: Manifold -> Bool
+isStart Start = True
+isStart _ = False
+
+charToManifold :: Char -> Maybe Manifold
+charToManifold 'S' = Just Start
+charToManifold '^' = Just Split
+charToManifold _ = Nothing
+
 runEasy :: FilePath -> IO String
-runEasy _ = return ""
+runEasy fp = do
+    grid <- parseFast parseGrid fp
+    let start = fst $ M.elemAt 0 $ M.filter isStart grid
+    let bottom = max $ map fst $ M.keys grid
+    return $ show $ sum $ fst $ foldl (expand grid) (0, S.singleton $ snd start) [fst start..bottom]
+
+expand :: Grid Manifold -> (Int, S.Set Int) -> Int
+expand grid (tot, streams) row
 
 runHard :: FilePath -> IO String
 runHard _ = return ""
+
+parseGrid :: String -> Grid Manifold
+parseGrid = constructGridSkipEmpty . ((fmap . fmap) charToManifold) . lines
