385c60802bea8d9aff0a346c534d4f0afdc64f2c
Rainbacon <reynolds.aaron1992@gmail.com>
2025-12-08 19:31:18 -0500

    more wip day 7

--- src/Problems2025/Problem7.hs
+++ src/Problems2025/Problem7.hs
@@ -7,30 +7,78 @@ import Text.Megaparsec.Char
 import Data.Void
 import qualified Data.Map as M
 import qualified Data.Set as S
+import qualified Control.Monad.State as ST 
+import Debug.Trace
 
-data Manifold = Start | Split
+data Manifold = Start | Split | Empty
+
+type ManifoldState = (Grid Manifold, S.Set Point, S.Set Point)
+type ManifoldState' = (Grid Manifold, [Point], M.Map Point Int)
 
 isStart :: Manifold -> Bool
 isStart Start = True
 isStart _ = False
 
-charToManifold :: Char -> Maybe Manifold
-charToManifold 'S' = Just Start
-charToManifold '^' = Just Split
-charToManifold _ = Nothing
+charToManifold :: Char -> Manifold
+charToManifold 'S' = Start
+charToManifold '^' = Split
+charToManifold '.' = Empty
+charToManifold c = error ("No manifold constructor matches char " ++ [c])
 
 runEasy :: FilePath -> IO String
 runEasy fp = do
     grid <- parseFast parseGrid fp
     let start = fst $ M.elemAt 0 $ M.filter isStart grid
-    let bottom = max $ map fst $ M.keys grid
-    return $ show $ sum $ fst $ foldl (expand grid) (0, S.singleton $ snd start) [fst start..bottom]
+    let initialState = (grid, (S.singleton start), S.empty)
+    return $ show $ ST.evalState runManifold initialState
 
-expand :: Grid Manifold -> (Int, S.Set Int) -> Int
-expand grid (tot, streams) row
+runManifold :: ST.State ManifoldState Int
+runManifold = do
+    (grid, tachyons, seen) <- ST.get
+    case S.size tachyons of
+        0 -> return 0
+        otherwise -> do
+            next <- popTachyon
+            splits <- expandTachyon next
+            rest <- runManifold
+            return $ splits + rest
+
+expandTachyon :: Point -> ST.State ManifoldState Int
+expandTachyon p@(x, y) = do
+    (grid, tachyons, seen) <- ST.get
+    case S.member p seen of
+        True -> return 0
+        False -> do
+            let next = (x + 1, y)
+            case M.lookup next grid of
+                Nothing -> return 0
+                (Just Split) -> do
+                    ST.put (grid, S.union tachyons (S.fromList [(x + 1, y - 1), (x + 1, y + 1)]), S.insert p seen)
+                    return 1
+                otherwise -> do
+                    ST.put (grid, S.insert next tachyons, S.insert p seen)
+                    return 0
+
+
+popTachyon :: ST.State ManifoldState Point
+popTachyon = do
+    (grid, tachyons, seen) <- ST.get
+    case S.size tachyons of
+        0 -> error "popped from empty list"
+        otherwise -> do
+            ST.put (grid, S.deleteAt 0 tachyons, seen)
+            return $ S.elemAt 0 tachyons
 
 runHard :: FilePath -> IO String
-runHard _ = return ""
+runHard fp = do
+    grid <- parseFast parseGrid fp
+    let start = fst $ M.elemAt 0 $ M.filter isStart grid
+    return $ show $ runManifold' grid start
+
+runManifold' :: Grid Manifold -> Point -> Int
+runManifold' g p = M.findWithDefault 1 p $ M.mapWithKey runManifold'' g
+    where runManifold'' (x, y) Split = runManifold' g (x, y - 1) + runManifold' g (x, y + 1) 
+          runManifold'' (x, y) _ = runManifold' g (x + 1, y)
 
 parseGrid :: String -> Grid Manifold
-parseGrid = constructGridSkipEmpty . ((fmap . fmap) charToManifold) . lines
+parseGrid = constructGrid . ((fmap . fmap) charToManifold) . lines
